f666abc10304d0ecf938019dc2f7f29597f4a303
IngridBondevik <ingridbondevik@outlook.com>
2025-01-09 15:48:32 +0100

    første del av prePros-funksjon

--- R/dataOps.R
+++ R/dataOps.R
@@ -14,3 +14,40 @@ getFirstRegistrations <- function(timepointData) {
 
   return(filteredData)
 }
+
+
+#' @export
+
+prePros <- function(data) {
+
+  data <- data %>%
+    dplyr::rename(Sykehus = db_unit_title,
+                  Kjønn = PatientGender,
+                  Alder = PatientAge)
+
+  data <- data %>%
+    dplyr::mutate(behandlingsstatus_ny = dplyr::recode(behandlingsstatus, "0" = "velg verdi", "1" = "aktiv",
+                                           "2" = "overført annen juridisk enhet",
+                                           "3" = "avbrutt", "4" = "avsluttet med gjensidig avtale",
+                                           "5" = "fulgt opp etter 1 år", "6" = "ikke oppnådd kontakt etter 1 år",
+                                           "7" = "ønsker ikke å svare etter 1 år"),
+
+                  behandlingsplan = dplyr::recode(plan_beh, "0" = "velg verdi", "1" = "nei", "2" = "ja", "3" = "vet ikke"),
+
+                  utbytte_evaluaring = dplyr::recode(eval_utbytte, "0" = "velg verdi", "1" = "meget dårlig", "2" = "dårlig",
+                                           "3" = "verken god eller dårlig", "4" = "god", "5" = "meget god/godt"))
+
+
+  data <- data %>%
+    dplyr::select(!behandlingsstatus) %>%
+    dplyr::rename(behandlingsstatus = behandlingsstatus_ny)
+
+  return(data)
+
+}
+
+# test for å se at det fungerer
+## f <- prePros(data_kvarus)
+
+# test for å se at det fungerer
+## f$behandlingsstatus
