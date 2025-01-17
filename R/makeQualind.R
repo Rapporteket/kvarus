4edd2df88f4e4d71afdef11ff883bc3d41ad6f54
IngridBondevik <ingridbondevik@outlook.com>
2025-01-17 16:08:28 +0100

    lintet makeQualind.R

--- R/makeQualind.R
+++ R/makeQualind.R
@@ -2,32 +2,32 @@
 
 kval_count <- function(punktData, var) { # legg evt. til flere variabler her avhengig av brukervalg
 
-  # Prosessindikatorene med KUN: første målepunkt ###############################
+  # Prosessindikatorene med KUN: første målepunkt ##############################
 
-  ###### ONLY FIRST POINT OF MEASURE ############################################
+  ###### ONLY FIRST POINT OF MEASURE ###########################################
   data <- getFirstRegistrations(punktData)
 
   data <- data %>%
-    dplyr::filter(behandlingsstatus == "aktiv")
+    dplyr::filter(.data$behandlingsstatus == "aktiv")
 
-  #data <- data %>%
-   # filter(Sykehus != "NA")
+  #data <- data %>% # nolint
+  #  filter(Sykehus != "NA") # nolint
 
-  ##### Make tiny data set with counts ###########################################
+  ##### Make tiny data set with counts #########################################
   my_tiny_data <- data %>%
     dplyr::add_tally(name = "alle") %>% # antall pasienter i datasettet
-    dplyr::group_by(Sykehus) %>%
+    dplyr::group_by(.data$Sykehus) %>%
     dplyr::add_tally(name = "per_syk") %>% # antall pasienter per sykehus
     dplyr::ungroup() %>%
-    dplyr::select(Sykehus, alle, per_syk) %>%
+    dplyr::select(.data$Sykehus, .data$alle, .data$per_syk) %>%
     dplyr::distinct()
 
 
-  ###### Filter based on kvalitetsindikatorer ####################################
+  ###### Filter based on kvalitetsindikatorer ##################################
 
   kval <- data %>%
     dplyr::filter(dplyr::case_when({{var}} == "behandlingsplan" ~
-                                    behandlingsplan == "ja",
+                                     behandlingsplan == "ja",
                                    {{var}} == "kriseplan" ~
                                      kriseplan == "ja",
                                    {{var}} == "utbytte" ~
@@ -35,35 +35,36 @@ kval_count <- function(punktData, var) { # legg evt. til flere variabler her avh
                                    ## Legg evt. til flere variabler her
                                    TRUE ~
                                      behandlingsplan != "ja")) %>%
-    dplyr::group_by(Sykehus) %>%
+    dplyr::group_by(.data$Sykehus) %>%
     dplyr::count(name = "antall_kval_syk") %>%
     dplyr::ungroup()
 
-  ##### Join data with counts based on kvalitetsindikator ########################
-  ###### with data based on whole data set #######################################
+  ##### Join data with counts based on kvalitetsindikator ######################
+  ###### with data based on whole data set #####################################
 
   jak <- dplyr::left_join(my_tiny_data, kval)
 
   jak <- jak %>%
-    dplyr::mutate(antall_kval_syk = tidyr::replace_na(antall_kval_syk, 0))
+    dplyr::mutate(antall_kval_syk = tidyr::replace_na(.data$antall_kval_syk, 0))
 
 
 
   magnus <- jak %>%
     dplyr::mutate(
-      andel_per_syk =
-        round(antall_kval_syk/per_syk*100, 2))
+      andel_per_syk = round(.data$antall_kval_syk / .data$per_syk * 100, 2)
+    )
 
 
   return(magnus)
 }
-
+# nolint start
 # testing:
-## kval <-  kval_count(punktData, "utbytte")
+## kval <-  kval_count(punktData, "utbytte") # nolint
 
 # Test for å sjekke:
 ## k <- kval_count(punktData, "behandlingsplan")
 
+# nolint end
 
 #' Annotations
 #'
@@ -82,90 +83,86 @@ kval_count <- function(punktData, var) { # legg evt. til flere variabler her avh
 #'
 #' @export
 
-annotations <- function(var){
-  anno = data.frame(xmax = 100, # in most cases xmax would be 100%
-                    xmin = 0, # this must be altered
-                    xmax_moderate = 0, # this must be altered
-                    xmin_moderate = 0) # this must be altered
+annotations <- function(var) {
+  anno <- data.frame(xmax = 100, # in most cases xmax would be 100%
+                     xmin = 0, # this must be altered
+                     xmax_moderate = 0, # this must be altered
+                     xmin_moderate = 0) # this must be altered
 
   anno <- anno %>%
     dplyr::mutate(xmin = dplyr::case_when({{var}} == "behandlingsplan" ~ 80,
                                           {{var}} == "kriseplan" ~ 60,
                                           {{var}} == "utbytte" ~ 80),
-                  xmax_moderate = xmin,
-                  xmin_moderate = dplyr::case_when({{var}} == "behandlingsplan" ~ 60,
-                                                   {{var}} == "kriseplan" ~ 40,
-                                                   {{var}} == "utbytte" ~ 60)
+      xmax_moderate = .data$xmin,
+      xmin_moderate = dplyr::case_when({{var}} == "behandlingsplan" ~ 60,
+                                       {{var}} == "kriseplan" ~ 40,
+                                       {{var}} == "utbytte" ~ 60)
     )
-
 
   return(anno)
 }
 
+# nolint start
 # Test what it works::
 ## anno <- annotations("kriseplan")
-
-
 
 # This function takes data frames made by other functions as input
 # "data" is the data frame made by kval_count
 # "gg_data" is the data frame made by makeGGdata
 # "anno" is the data frame made by "annotations"
 
+# nolint end
+
 #' @export
 
-kval_plot <- function(data, ggData, anno){
+kval_plot <- function(data, ggData, anno) {
 
   kval_plot <- data %>%
+    ggplot2::ggplot(ggplot2::aes(x = .data$andel_per_syk, y = .data$Sykehus)) +
 
-      ggplot2::ggplot(ggplot2::aes(x = andel_per_syk, y = Sykehus))+
+    ggplot2::annotate("rect", ######### DENNE KAN HELLER BRUKES "OVER TID"...
+                      xmin = anno$xmin,
+                      xmax = anno$xmax,
+                      ymin = -Inf, ymax = Inf, fill = "lightgreen",
+                      alpha = .25) +
 
-      ggplot2::annotate("rect", ######### DENNE KAN HELLER BRUKES "OVER TID"...
-                        xmin = anno$xmin,
-                        xmax = anno$xmax,
-                        ymin = -Inf, ymax = Inf , fill = "lightgreen",
-                        alpha = .25)+
+    ggplot2::annotate("rect", ######### DENNE KAN HELLER BRUKES "OVER TID"...
+                      xmin = anno$xmin_moderate,
+                      xmax = anno$xmax_moderate,
+                      ymin = -Inf, ymax = Inf, fill = "gold",
+                      alpha = .15) +
 
-      ggplot2::annotate("rect", ######### DENNE KAN HELLER BRUKES "OVER TID"...
-                        xmin = anno$xmin_moderate,
-                        xmax = anno$xmax_moderate,
-                        ymin = -Inf, ymax = Inf , fill = "gold",
-                        alpha = .15)+
+    ggplot2::geom_col(fill = "#6CACE4", alpha = .7) +
 
+    # ggplot2::geom_rect(aes(ymin = 0, ymax = 5, xmin = x_start, xmax = x_end), alpha = .5)+
 
-      ggplot2::geom_col(fill = "#6CACE4", alpha = .7)+
+    #### TITLES ################################################################
 
-      # ggplot2::geom_rect(aes(ymin = 0, ymax = 5, xmin = x_start, xmax = x_end), alpha = .5)+
+    ggplot2::xlab(ggData$xlab) +
 
-      #### TITLES ################################################################
+    ggplot2::ylab("Sykehus") +
 
-      ggplot2::xlab(ggData$xlab)+
+    ggplot2::ggtitle(ggData$title) +
 
-      ggplot2::ylab("Sykehus")+
+    ggplot2::geom_label(ggplot2::aes(x = 0, label = paste(.data$antall_kval_syk, "av", .data$per_syk)),
+                        fill = "#BFCED6", color = "#003087", fontface = "italic",
+                        position = ggplot2::position_dodge(.9), vjust = .5, size = 3,
+                        alpha = .8) +
 
-      ggplot2::ggtitle(ggData$title)+
-
-
-      ggplot2::geom_label(ggplot2::aes(x = 0, label = paste(antall_kval_syk, "av", per_syk)),
-                          fill = "#BFCED6", color = "#003087", fontface = "italic",
-                          position = ggplot2::position_dodge(.9), vjust = .5, size = 3,
-                          alpha = .8)+
-
-    ggplot2::scale_x_continuous(breaks = c(20,40,60,80,100))+ # maybe alter this based if other variables are chosen
+    ggplot2::scale_x_continuous(breaks = c(20, 40, 60, 80, 100)) +
+    # maybe alter this based if other variables are chosen
 
 
-      ##### THEME AND COLOURS ####################################################
+    ##### THEME AND COLOURS ####################################################
 
     ggplot2::theme_light() # light theme
 
-
-
-    return(kval_plot)
-  }
+  return(kval_plot)
+}
 
 
 # Test to see that it works
-## kval_plot(kval, ggData, anno)
+## kval_plot(kval, ggData, anno) # nolint
 
 
 #' @title Function for getting explanations for kvalitetsindikatorer
