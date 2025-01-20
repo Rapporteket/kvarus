399fb9c96d01b72f65d5cc426fabafcaba591e18
IngridBondevik <ingridbondevik@outlook.com>
2025-01-20 15:07:39 +0100

    Lagt makeGGdata inn i makeQualind.R

--- R/makeQualind.R
+++ R/makeQualind.R
@@ -119,6 +119,28 @@ annotations <- function(var) {
 
 # nolint end
 
+#'@export
+makeGGdata <- function(var, type_plot) {
+  ggdata <- data.frame(title = "", xlab = "")
+
+  config <- get_config()
+
+  if (type_plot == "kval") {
+    ggdata <- ggdata |>
+      dplyr::mutate(
+        title = dplyr::case_when({{var}} == "behandlingsplan" ~ config$kvalind$behandlingsplan$tittel,
+                                 {{var}} == "kriseplan" ~ config$kvalind$kriseplan$tittel,
+                                 {{var}} == "utbytte" ~ config$kvalind$utbytte$tittel,
+                                 {{var}} == "gjensidig" ~ config$kvalind$gjensidig$tittel,
+                                 .default = config$kvalind$default$tittel),
+        xlab = "Andel pasienter"
+      )
+  }
+
+  return(ggdata)
+}
+
+
 #' @export
 
 kval_plot <- function(data, ggData, anno) {
