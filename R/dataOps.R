bf098cd56f3d27d3ed7f36cb485a453dfb0c8884
IngridBondevik <ingridbondevik@outlook.com>
2025-01-24 15:24:11 +0100

    Laget funksjon for basic utvalg

--- R/dataOps.R
+++ R/dataOps.R
@@ -15,6 +15,11 @@ getFirstRegistrations <- function(timepointData) {
   return(filteredData)
 }
 
+#' Pre-process the dataset
+#'
+#' This function relevels the different levels of the columns and changes
+#' numbers into readble texts etc.
+#'
 #' @export
 
 prePros <- function(data) {
@@ -46,14 +51,72 @@ prePros <- function(data) {
                                 "3" = "vet ikke"),
 
 
-      Kjønn = dplyr::recode(.data$PatientGender,
-                            "1" = "mann",
-                            "2" = "kvinne")
-    ) # Vill gjetning
+      gender = dplyr::recode(.data$PatientGender,
+                             "1" = "mann",
+                             "2" = "kvinne")
+    )
 
   data <- data |>
     dplyr::select(!.data$behandlingsstatus) |>
     dplyr::rename(behandlingsstatus = .data$behandlingsstatus_ny)
 
+  data$dato_mp_beh <-as.Date(data$dato_mp_beh)
+
   return(data)
 }
+
+#'Sample the data
+#'
+#'This function extracts samples from the large dataset. This function works
+#'on datasets that have been preprosessed by prePros.
+#'Sorting on gender, status, date of treatment measuring point, age of patient and unit.
+#'
+#'@export
+
+basicSample <- function (data, gender, status, time1, time2, age1, age2, unit) {
+
+    # Filter by gender
+
+    data <- data |>
+      dplyr::filter(dplyr::case_when({{gender}} == "kvinne" ~ gender == "kvinne",
+                                     {{gender}} == "mann" ~ gender == "mann",
+                                     {{gender}} != "kvinne" |
+                                       {{gender}} != "mann" ~ gender %in% c("kvinne", "mann")))
+
+    # Filter by operation type
+
+  data <- data |>
+    dplyr::filter(behandlingsstatus == dplyr::case_when({{status}} == "aktiv" ~ "aktiv",
+                                                        {{status}} == "avstluttet med gjensidig avtale" ~ "avsluttet med gjensidig avtale",
+                                                        {{status}} == "avbrutt" ~ "avbrutt",
+                                                        {{status}} == "overført" ~ "overført annen juridisk enhet",
+                                                        {{status}} == "fulgt opp" ~ "fulgt opp etter 1 år",
+                                                        {{status}} == "ikke oppnådd kontakt" ~ "ikke oppnådd kontakt etter 1 år"))
+
+    # Add filter on surgery date------------------------------------------------
+
+    data <- data|>
+      dplyr::filter(dplyr::between(dato_mp_beh,
+                                   as.Date({{time1}}),
+                                   as.Date({{time2}})))
+
+    # Add filter on age---------------------------------------------------------
+
+    # Using column "Alder_num" in which alder is given as an integer
+
+    data <- data |>
+      dplyr::filter(dplyr::between(Alder,
+                                   {{age1}},
+                                   {{age2}}))
+
+
+    # Unit ---------------------------------------------------------------------
+
+    data <- data |>
+      dplyr::filter(Sykehus == {{unit}})
+
+
+    return (data)
+
+  }
+
