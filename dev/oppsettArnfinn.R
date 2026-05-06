69d95b76fc67ed8ef135349ff626ee53050fcac0
Arnfinn Hykkerud Steindal <arnfinn.hykkerud.steindal@helse-nord.no>
2026-05-06 10:32:13 +0200

    dev-greier
    Kode for å snurre opp app lokalt.

---
+++ dev/oppsettArnfinn.R
@@ -0,0 +1,12 @@
+sship::dec(
+  "c://Users/ast046/Downloads/kvarus1371ab41b.sql.gz__20260506_093906.tar.gz",
+  keyfile = "c://Users/ast046/.ssh/id_rsa",
+  target_dir = "c://Users/ast046/Downloads/")
+
+devtools::install("../rapbase/.", dependencies = FALSE)
+devtools::install(dependencies = FALSE)
+
+source("dev/sysSetenv.R")
+kvarus::run_app(browser = TRUE)
+
+units <- jsonlite::fromJSON(Sys.getenv("MRS_ACCESS_HIERARCHY_URL"))$AccessUnits
