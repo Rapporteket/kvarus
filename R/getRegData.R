d92545db1d019b7cde0b241201ae25a9171d1e46
Arnfinn Hykkerud Steindal <arnfinn.hykkerud.steindal@helse-nord.no>
2026-05-20 16:43:41 +0200

    Kvalitetsindikator: hent kun nødvendige data fra db
    Navnendring i query i tillegg.

--- R/getRegData.R
+++ R/getRegData.R
@@ -18,7 +18,19 @@ getBasisData <- function() {
 
 getTimepointData <- function() {
 
-  query <- "SELECT * FROM maalepunkt_2;"
+  query <- "SELECT
+   PasientGUID,
+   dato_mp_beh,
+   UnitId,
+   db_unit_title as Sykehus,
+   PatientAge as Alder,
+   behandlingsstatus,
+   plan_beh,
+   plan_krise,
+   eval_utbytte,
+   PatientGender
+   FROM maalepunkt_2;
+  "
 
   rapbase::loadRegData("data", query)
 
