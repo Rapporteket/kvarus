7890f1a47de24c1a502df1b3f7baadd45992dfd6
Arnfinn Hykkerud Steindal <arnfinn.hykkerud.steindal@helse-nord.no>
2026-05-21 16:04:26 +0200

    fiks test

--- tests/testthat/test_makeHist.R
+++ tests/testthat/test_makeHist.R
@@ -1,40 +1,24 @@
 test_that("makeHist returns a histogram object", {
     df <- mtcars
     var <- "mpg"
-    bins <- 5
-    result <- makeHist(df = df, var = var, bins = bins, makeTable = FALSE)
+    result <- makeHist(df = df, var = var)
 
-    expect_true(inherits(result, "histogram"))
-    expect_equal(result$breaks, c(10.4, 15.1, 19.8, 24.5, 29.2, 33.9))
+    expect_true(inherits(result, "ggplot"))
+    expect_equal(unique(ggplot2::get_layer_data(result)$y), c(2, 1))
 })
 
-test_that("makeHist returns a data frame when makeTable is TRUE", {
-    df <- mtcars
-    var <- "mpg"
-    bins <- 5
-    result <- makeHist(df = df, var = var, bins = bins, makeTable = TRUE)
-
-    expect_true(is.data.frame(result))
-    expect_equal(ncol(result), 3)
-    expect_named(result, c("GruppeMin", "GruppeMax", "Antall"))
-})
 
 test_that("makeHist handles NA values correctly", {
     df <- mtcars
     df$mpg[1:3] <- NA
     var <- "mpg"
-    bins <- 5
-    result <- makeHist(df = df, var = var, bins = bins, makeTable = TRUE)
+    result <- makeHist(df = df, var = var)
+    expect_true(inherits(result, "ggplot"))
 
-    expect_true(all(!is.na(result$Antall)))
-})
-
-test_that("makeHist produces correct binning", {
-    df <- mtcars
-    var <- "mpg"
-    bins <- 4
-    result <- makeHist(df = df, var = var, bins = bins, makeTable = TRUE)
-
-    expect_equal(nrow(result), bins)
-    expect_true(all(result$GruppeMin < result$GruppeMax))
+    expect_warning(
+      expect_equal(
+        unique(ggplot2::get_layer_data(result)$y),
+        c(2, 1)
+      )
+    )
 })
