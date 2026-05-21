655d95234ba465693a35fcf59a03d482d97a28d8
Arnfinn Hykkerud Steindal <arnfinn.hykkerud.steindal@helse-nord.no>
2026-05-21 16:15:08 +0200

    litt mer test

--- tests/testthat/test_makeHist.R
+++ tests/testthat/test_makeHist.R
@@ -2,9 +2,16 @@ test_that("makeHist returns a histogram object", {
     df <- mtcars
     var <- "mpg"
     result <- makeHist(df = df, var = var)
+    layer_data <- ggplot2::get_layer_data(result)
 
     expect_true(inherits(result, "ggplot"))
-    expect_equal(unique(ggplot2::get_layer_data(result)$y), c(2, 1))
+    # antallet er enten 1 eller 2
+    expect_equal(unique(layer_data$y), c(2, 1))
+    expect_equal(nrow(layer_data), 25)
+    labs <- ggplot2::get_labs(result)
+    expect_equal(labs$x, "mpg")
+    expect_equal(labs$y, "Antall")
+    expect_equal(labs$title, "Fordeling av mpg")
 })
 
 
